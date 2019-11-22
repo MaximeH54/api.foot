2e2447f65f00721e19c89222464637a93e2ae074
Maxime <maxime_hemmerle@orange.fr>
2019-11-22 16:52:15 +0100

    api finish

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('ornytogwaaakkkk !!!');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('new message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3045, function(){
+  console.log('listening on *:3045');
+});
